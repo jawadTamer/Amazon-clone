94d71e5f5b06b2c4a2af23b77969bbfe0a24c9c8
salmasaeed90 <salmaasaeed90@gmail.com>
2025-03-13 09:40:44 +0200

    add ts file

--- src/app/components/product-details/product-details.component.ts
+++ src/app/components/product-details/product-details.component.ts
@@ -1,12 +1,42 @@
 import { Component } from '@angular/core';
+import { Product, ProductsService } from '../../shared/service/products.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-details',
   standalone: true,
-  imports: [],
+  imports: [UpperCasePipe],
   templateUrl: './product-details.component.html',
   styleUrl: './product-details.component.css'
 })
 export class ProductDetailsComponent {
+  productId!:any;
+  productDetails!:Product;
+  constructor(
+    private _ActivatedRoute:ActivatedRoute,
+    private _ProductsService:ProductsService
+  ){}
+
+  ngOnInit():void{
+    this._ActivatedRoute.paramMap.subscribe({
+      next:(params)=>{
+        this.productId = params.get('id')
+      }
+    })
+
+    this.getProductDetails();
+  }
+  getProductDetails(){
+    this._ProductsService.getProductById(11).subscribe({
+      next:(response)=>{
+        console.log(response);
+        this.productDetails = response;
+      },
+      error:(err)=>{
+        console.log(err);
+        
+      }
+    })
+  }
 
 }
